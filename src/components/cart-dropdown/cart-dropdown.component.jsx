640ac5a606fcc0e7a3ce372217f01e378f1ab326
TechRanga <TechRanga@github.com>
2020-03-18 04:15:52 +0530

    Created Cart Item component to display on cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
+import CartItem from '../cart-item/cart-item.component';
+import {connect} from 'react-redux';
 
-const Cart =()=>(
+const Cart =({cartItems})=>(
     <div className='cart-dropdown'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
+            {
+                cartItems.map(
+                    cartItem => (
+                        <CartItem key={cartItem.id} item={cartItem} />
+                    )
+                )
+            }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-export default Cart;
+const mapStateToProps = ({cart:{cartItems}}) => (
+    {
+        cartItems
+    }
+);
+
+export default connect(mapStateToProps)(Cart);
 
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -8,9 +8,11 @@ const CollectionItem=({item,addItem})=>{
     const {name,price,imageUrl} = item;
     return (
         <div className='collection-item'>
-            <div className='image'
-            style={{backgroundImage:`url${imageUrl}`}}
-            />
+            <div className='image'>
+                <img src={imageUrl} alt='item'/>
+            </div>
+            
+            
             <div className='collection-footer'>
                 <span className='name'>{name}</span>
                 <span className='price'>${price}</span>
