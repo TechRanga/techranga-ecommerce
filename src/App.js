9938bb3bbd2f4de8772155d44b59232695686797
TechRanga <TechRanga@github.com>
2020-03-17 23:50:09 +0530

    Added Action Types and Redirect Feature post sign in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch,Route} from 'react-router-dom';
+import {Switch,Route,Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import './App.css';
 
@@ -42,7 +42,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
-          <Route path='/signIn' component={SignInAndSignUpPage}/>
+          <Route exact path='/signIn' render={()=>this.props.currentUser?(<Redirect to='/'/>):(<SignInAndSignUpPage/>)}/>
         </Switch>
       </div>
     );
@@ -58,4 +58,11 @@ const mapDispatchToProps = dispatch =>(
     setCurrentUser: user=>dispatch(setCurrentUser(user))
   }
 );
-export default connect(null,mapDispatchToProps)(App);
+
+const mapStateToProps = ({user}) =>(
+  {
+    currentUser:user.currentUser
+  }
+);
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
