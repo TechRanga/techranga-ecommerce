c02917822548f5ec3ac09af24cb17fb49850446e
TechRanga <TechRanga@github.com>
2020-03-21 01:58:33 +0530

    Modified SHOP_DATA to handle through selectors

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
-import {selectCollections} from '../../redux/shop/shop.selector';
+import {selectCollectionsForPreview} from '../../redux/shop/shop.selector';
 import './collections-overview.styles.scss';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
@@ -22,7 +22,7 @@ const CollectionsOverview = ({collections}) => (
 
 const mapStateToProps = createStructuredSelector(
     {
-        collections:selectCollections
+        collections:selectCollectionsForPreview
     }
 )
 
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,12 +1,5 @@
 import {createSelector} from 'reselect';
 
-const COLLETION_ID_MAP = {
-    hats:1,
-    jackets:2,
-    sneakers:3,
-    womens:4,
-    mens:5
-};
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -14,11 +7,14 @@ export const selectCollections = createSelector(
     shop => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key=>collections[key])
+);
+
 export const selectCollection = collectionURLParam => 
     createSelector(
         [selectCollections],
         collections =>
-            collections.find(
-                collection => collection.id === COLLETION_ID_MAP[collectionURLParam]
-            )
+            collections[collectionURLParam]
     );
