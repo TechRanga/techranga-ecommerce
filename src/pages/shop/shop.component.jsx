93a34fdf0d75b6f953bb97bbaed71c01f88e8fbe
TechRanga <TechRanga@github.com>
2020-03-23 04:42:35 +0530

    Moved Collection Overview and Collections to containers

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,67 +1,58 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import CollectionOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
 import {connect} from 'react-redux';
-import {firestore,covertCollectionSnapshotToMap} from '../../firebase/firebase.utils';
-import {updateCollections} from '../../redux/shop/shop.actions';
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
-
-const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+import {fetchCollectionAsync} from '../../redux/shop/shop.actions';
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from '../collection/collection.container';
 
 class ShopPage extends React.Component {
 
-    state ={loading:true};
-
-    unsubscribeFromSnapshot = null;
-
     componentDidMount(){
-        const {updateCollections} = this.props
-        const collectionRef = firestore.collection('collections');
-        //Observable pattern:
-        // collectionRef.onSnapshot(
-        //     async snapshot => {
-        //        const collectionsMap  = covertCollectionSnapshotToMap(snapshot);
-        //        updateCollections(collectionsMap);
-        //        this.setState({loading:false})
-        //     }
-        // );
-
-        //Promise pattern
-        collectionRef.get().then(
-            async snapshot => {
-                const collectionsMap  = covertCollectionSnapshotToMap(snapshot);
-                updateCollections(collectionsMap);
-                this.setState({loading:false})
-             }
-        );
-
-        //Fetch pattern -> Returns a very nested object containing the data if using NoSQL.
-        //https://firestore.googleapis.com/v1/projects/techranga-ecommerce-db/databases/(default)/documents/collections
-        // fetch('https://firestore.googleapis.com/v1/projects/techranga-ecommerce-db/databases/(default)/documents/collections')
-        // .then(response => response.json())
-        // .then(collections => console.log(collections));
+        const {fetchCollectionAsync} = this.props;
+        fetchCollectionAsync();
     };
 
     render(){
         const {match} = this.props;
-        const {loading} = this.state;
         return(
             <div className='shope-page'>
-                <Route exact path={`${match.path}`} render={(props)=><CollectionOverviewWithSpinner isLoading={loading} {...props}/>} />
-                <Route path={`${match.path}/:collectionId`} render={(props)=><CollectionPageWithSpinner isLoading={loading} {...props}/>} />
+                <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+                <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
             </div>
-        )
-    }
+        )};
 };
 
 const mapDispatchToProps = dispatch => (
     {
-        updateCollections:collectionsMap=>dispatch(updateCollections(collectionsMap))
+        fetchCollectionAsync:()=>dispatch(fetchCollectionAsync())
     }
 );
 
-
-
 export default connect(null,mapDispatchToProps)(ShopPage);
+
+
+//const {updateCollections} = this.props
+//const collectionRef = firestore.collection('collections');
+//Observable pattern:
+// collectionRef.onSnapshot(
+//     async snapshot => {
+//        const collectionsMap  = covertCollectionSnapshotToMap(snapshot);
+//        updateCollections(collectionsMap);
+//        this.setState({loading:false})
+//     }
+// );
+
+//Promise pattern
+// collectionRef.get().then(
+//     async snapshot => {
+//         const collectionsMap  = covertCollectionSnapshotToMap(snapshot);
+//         updateCollections(collectionsMap);
+//         this.setState({loading:false})
+//      }
+// );
+
+//Fetch pattern -> Returns a very nested object containing the data if using NoSQL.
+//https://firestore.googleapis.com/v1/projects/techranga-ecommerce-db/databases/(default)/documents/collections
+// fetch('https://firestore.googleapis.com/v1/projects/techranga-ecommerce-db/databases/(default)/documents/collections')
+// .then(response => response.json())
+// .then(collections => console.log(collections));
--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,8 +1,45 @@
 import ShopActionTypes from './shop.actionTypes';
+import {firestore,covertCollectionSnapshotToMap} from '../../firebase/firebase.utils'
+// export const updateCollections = collectionsMap => (
+//     {
+//         type:ShopActionTypes.UPDATE_COLLETIONS,
+//         payload:collectionsMap
+//     }
+// )
 
-export const updateCollections = collectionsMap => (
+export const startFetchCollection = () => (
     {
-        type:ShopActionTypes.UPDATE_COLLETIONS,
-        payload:collectionsMap
+        type:ShopActionTypes.FETCH_COLLECTIONS_START
+    }
+);
+
+const fetchCollectionsSuccess = collections => (
+    {
+        type:ShopActionTypes.FETCH_COLLETIONS_SUCCESS,
+        payload:collections
     }
 )
+
+const fetchCollectionError = error => (
+    {
+        type:ShopActionTypes.FETCH_COLLETIONS_FAILURE,
+        payload:error
+    }
+)
+
+export const fetchCollectionAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(startFetchCollection());
+
+        collectionRef.get().then(
+            async snapshot => {
+                const collectionsMap  = covertCollectionSnapshotToMap(snapshot);
+                dispatch(fetchCollectionsSuccess(collectionsMap));
+             }).catch(
+                 error => dispatch(fetchCollectionError(error.message))
+             );
+
+    }
+};
+
