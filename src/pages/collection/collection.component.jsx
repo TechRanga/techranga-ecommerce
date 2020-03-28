247b597dcdb385ddcb248a4319e6e0b0afe9ebe4
TechRanga <TechRanga@github.com>
2020-03-29 03:49:47 +0530

    Added styling to collection view
    Added styling to collection view

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,19 +5,23 @@ import {selectCollection} from '../../redux/shop/shop.selector';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
+import {
+    CollectionPageContainer,
+    CollectionTitle,
+    CollectionItemsContainer
+  } from './collection.styled';
+
 const CollectionPage = ({collection}) => {
     const {title,items} = collection;
     return(
-        <div className='collection-page'>
-            <h2 className='title'>{title.toUpperCase()}</h2>
-            <div className='items'>
-            {
-                items.map(
-                    item=><CollectionItem key={item.id} item={item}/>
-                )
-            }
-            </div>
-        </div>
+        <CollectionPageContainer>
+            <CollectionTitle>{title}</CollectionTitle>
+            <CollectionItemsContainer>
+            {items.map(item => (
+                <CollectionItem key={item.id} item={item} />
+            ))}
+            </CollectionItemsContainer>
+        </CollectionPageContainer>
     )
 };
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import {Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {startFetchCollection} from '../../redux/shop/shop.actions';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
-class ShopPage extends React.Component {
+const ShopPage =({match,startFetchCollection})=> {
 
-    componentDidMount(){
-        const {startFetchCollection} = this.props;
-        startFetchCollection();
-    };
-
-    render(){
-        const {match} = this.props;
-        return(
-            <div className='shope-page'>
-                <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-                <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
-            </div>
-        )};
+    useEffect(
+        ()=>{
+            startFetchCollection();
+        },[startFetchCollection]
+      );
+    return(
+        <div className='shope-page'>
+            <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+        </div>
+    );
 };
 
 const mapDispatchToProps = dispatch => (
